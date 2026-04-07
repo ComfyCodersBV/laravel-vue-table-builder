993f54b9097f069032208a928b2df65ca2fe3c98
Arno Rietdijk <arno@comfycoders.nl>
2026-04-07 11:28:36 +0200

    [TB-5] Code styling and added form reqeust for bulk action route

--- src/Http/Controllers/TableBulkActionController.php
+++ src/Http/Controllers/TableBulkActionController.php
@@ -4,30 +4,26 @@ declare(strict_types=1);
 
 namespace TranquilTools\TableBuilder\Http\Controllers;
 
+use TranquilTools\TableBuilder\Http\Requests\BulkActionRequest;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Validation\UnauthorizedException;
 use TranquilTools\TableBuilder\AbstractTable;
 
 class TableBulkActionController extends Controller
 {
-    public function __invoke(Request $request): RedirectResponse
+    public function __invoke(BulkActionRequest $request): RedirectResponse
     {
-        $request->validate([
-            'ids' => ['required', 'array', 'min:1'],
-        ]);
-
         $action = base64_decode($request->input('action'));
 
         /** @var AbstractTable $tableInstance */
         $tableInstance = app(base64_decode($request->input('table')));
 
-        if (!$tableInstance->authorize($request)) {
+        if (! $tableInstance->authorize($request)) {
             throw new UnauthorizedException;
         }
 
-        $tableInstance->performBulkAction((int)$action, $request->input('ids', []));
+        $tableInstance->performBulkAction((int) $action, $request->input('ids', []));
 
         return redirect()->back();
     }
